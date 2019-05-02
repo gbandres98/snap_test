7a35ed950b57da201944765854a652a16bc6f19e
Guillermo Bandrés Catalina <mail@gbandres.com>
2019-05-02 12:47:23 +0200

    Usar time.Ticker

--- gohelloworldbabel.go
+++ gohelloworldbabel.go
@@ -9,22 +9,39 @@ import (
 )
 
 var path = filepath.FromSlash(fmt.Sprint(os.Getenv("SNAP_USER_DATA"), "/dummy.log"))
+var quit = make(chan struct{})
 
 func init() {
 	fmt.Println(path)
 }
 
 func main() {
+	ticker := time.NewTicker(1 * time.Second)
+	go func() {
+		for t := range ticker.C {
+			_ = t // we don't print the ticker time, so assign this `t` variable to underscore `_` to avoid error
+			if err := writeFile(); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	<-quit
+}
+
+func writeFile() error {
 	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
 
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer file.Close()
 
 	line := fmt.Sprintln(time.Now(), " - Snap: ", os.Getenv("SNAP_NAME"), " - Ver: ", os.Getenv("SNAP_REVISION"))
-	fmt.Println(line)
+	fmt.Print(line)
 	if _, err := file.WriteString(line); err != nil {
-		log.Fatal(err)
+		return err
 	}
+
+	return nil
 }
