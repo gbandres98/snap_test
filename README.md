069b9f9b9db0a9966fb54448850b82499c5dbb6d
Guillermo Bandrés Catalina <mail@gbandres.com>
2019-05-02 13:51:32 +0200

    Tomar el tiempo de un servidor externo

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # snap_test
-  
-
-
--- gohelloworldbabel.go
+++ gohelloworldbabel.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"time"
 )
+
+type TimeResponse struct {
+	Status    string `json:"status,omitempty"`
+	Formatted string `json:"formatted,omitempty"`
+}
 
 var path = filepath.FromSlash(fmt.Sprint(os.Getenv("SNAP_USER_DATA"), "/dummy.log"))
 var quit = make(chan struct{})
@@ -37,7 +44,21 @@ func writeFile() error {
 	}
 	defer file.Close()
 
-	line := fmt.Sprintln(time.Now(), " - Snap: ", os.Getenv("SNAP_NAME"), " - Ver: ", os.Getenv("SNAP_REVISION"))
+	r, err := http.Get("http://api.timezonedb.com/v2.1/get-time-zone?key=50SG1ZNZM5CR&by=zone&zone=Europe/Madrid&format=json")
+	var timeString string
+	if err != nil {
+		fmt.Println(err)
+		timeString = time.Now().Format("2006-01-02 15:04:05")
+	} else {
+		defer r.Body.Close()
+		timeResponse := &TimeResponse{}
+		if err := json.NewDecoder(r.Body).Decode(timeResponse); err != nil {
+			return err
+		}
+		timeString = timeResponse.Formatted
+	}
+
+	line := fmt.Sprintln(timeString, "- Snap: ", os.Getenv("SNAP_NAME"), "- Ver: ", os.Getenv("SNAP_REVISION"))
 	fmt.Print(line)
 	if _, err := file.WriteString(line); err != nil {
 		return err
